d114d19c81b906daa779f001e1385c5299fa27e5
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-08-31 22:43:03 +0300

    test: FFN-5 add test for first play

--- cypress/e2e/tasks.cy.ts
+++ cypress/e2e/tasks.cy.ts
@@ -1,6 +1,14 @@
 describe('Flexbox Froggy', () => {
-  it('Should validate tasks', () => {
+  it('Should open page', () => {
     cy.visit('https://flexboxfroggy.com');
     cy.get('.title').should('contain', 'Flexbox Froggy');
+  });
+
+  it('Should validate first task', () => {
+    cy.visit('https://flexboxfroggy.com');
+    cy.get('#level-indicator').click();
+    cy.get('.level-marker[data-level="0"]').click();
+    cy.get('#editor textarea').type('justify-content: flex-end;');
+    cy.get('#next').should('not.have.class', 'disabled');
   });
 });
